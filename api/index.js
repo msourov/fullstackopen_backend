821b18e2e1571f6b4b1f08ead31c5d579647982b
Mahmud Hasan Sourov <sourov1666@gmail.com>
2024-08-26 23:40:02 +0600

    changed structure

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("../dist"));
+app.use(express.static(path.join(__dirname, "../build")));
 
 let notes = [
   {
