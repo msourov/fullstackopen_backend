c7bee5c087837db1defe993ef8977df1ec94dfee
Mahmud Hasan Sourov <sourov1666@gmail.com>
2024-08-27 08:17:14 +0600

    m

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 let notes = [
   {
